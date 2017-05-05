79d1309f80cc0dee6fdd11cd678521f3d2101e61
CJ <weicongju@gmail.com>
2017-05-05 13:09:14 +0800

    Run command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,9 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        'App\Console\Commands\SupervisordCommand'
+        'App\Console\Commands\SupervisordCommand',
+        'App\Console\Commands\RunCommand',
+        'App\Console\Commands\ProcesTestCommand'
     ];
 
     /**
