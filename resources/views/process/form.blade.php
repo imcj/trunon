5341c153a9463eb5e368af481542396ef4570432
CJ <weicongju@gmail.com>
2017-05-07 22:51:29 +0800

    切换部署模式，隐藏和当前无关的表单项。

--- resources/views/process/form.blade.php
+++ resources/views/process/form.blade.php
@@ -14,7 +14,7 @@
                 <label for="code">Command</label>
                 <textarea class="form-control" name="command" placeholder="nginx">{{$process->command}}</textarea>
             </div>
-            <div class="form-group deploy_code" style="display: none">
+            <div class="form-group deploy_code" stype="display: none;">
                 <label for="code">Code</label>
                 <textarea class="form-control" name="code">{{$process->code}}</textarea>
             </div>
@@ -27,24 +27,32 @@
             {{$slot}}
 
             <script>
+
+function activeDeploy(activeType)
+{
+    var deploy = ['command', 'code'];
+    for (var i = 0, size = deploy.length; i < size; i++) {
+        var type = deploy[i];
+        var commandElement = $(".deploy_" + type);
+        var className = ".deploy_" + type
+        if (type == activeType)
+            $(className).css("display", "block");
+        else
+            $(className).css("display", "none");
+    }
+}
+
 $(document.body).ready(function() {
     var process =
     @php
     echo json_encode($process);
     @endphp
     
-
-    $("#deploy").val(process.deploy.toLowerCase());
-    var deploy = ['command', 'code'];
-    for (var i = 0, size = deploy.length; i < size; i++) {
-        var d = deploy[i];
-        var commandElement = $(".deploy_" . d);
-
-        if (d == process.command)
-            commandElement.css("display", "block");
-        else
-            commandElement.css("display", "none");
-    }
-    
+    $("#deploy").change(function(event) {
+        var currentDeployType = $(event.currentTarget).val()
+        activeDeploy(currentDeployType)
+    })
+    activeDeploy(process.deploy)
+    $("#deploy").val(process.deploy.toLowerCase())
 })
 </script>
